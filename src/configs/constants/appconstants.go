93d311a0e8b72d9f0f68e42bf9968432ac637f81
imshahbaz21 <imshahbaz21@gmail.com>
2025-03-21 22:43:08 +0530

    updating

--- src/configs/constants/appconstants.go
+++ src/configs/constants/appconstants.go
@@ -18,9 +18,9 @@ type strategy struct {
 }
 
 var Strategies = []strategy{
-	{Name: "Vollinger", Clause: "( {46553} ( latest close > latest open and latest close > latest sma( latest close , 20 ) and latest volume > 1 day ago volume and latest low <= latest sma( latest close , 20 ) and latest close >= ( ( latest high - latest low ) * 0.7 ) + latest low ) )", Active: false},
+	{Name: "Vollinger", Clause: "( {46553} ( latest close > latest open and latest close > latest sma( latest close , 20 ) and latest volume > 1 day ago volume and latest low <= latest sma( latest close , 20 ) and latest close >= ( ( latest high - latest low ) * 0.7 ) + latest low ) )", Active: true},
 	{Name: "Super Rsi", Clause: "( {46553} ( latest close > latest open and latest close > latest supertrend( 10 , 3 ) and latest rsi( 14 ) > 60 and 1 day ago  rsi( 14 ) <= 60 ) )", Active: true},
-	{Name: "EMACD", Clause: "( {46553} ( latest close > latest ema( latest close , 21 ) and latest macd line( 26 , 12 , 9 ) > latest macd signal( 26 , 12 , 9 ) and 1 day ago  macd line( 26 , 12 , 9 ) <= 1 day ago  macd signal( 26 , 12 , 9 ) ) )", Active: true},
+	{Name: "EMACD", Clause: "( {46553} ( latest close > latest ema( latest close , 21 ) and latest macd line( 26 , 12 , 9 ) > latest macd signal( 26 , 12 , 9 ) and 1 day ago  macd line( 26 , 12 , 9 ) <= 1 day ago  macd signal( 26 , 12 , 9 ) ) )", Active: false},
 }
 
 var GsheetMargin = make(map[string]dto.MarginDto)
